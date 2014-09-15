06ecb7473e6caa6775de04a9f683707f28e37f92
Marcus Thompson <marcust@email.unc.edu>
2014-09-15 16:01:22 -0400

    Displayed the navigation menu in the Appearance --> Menus panel using wp_nav_menu function.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -133,3 +133,25 @@ function marcus_thompson_category_transient_flusher() {
 }
 add_action( 'edit_category', 'marcus_thompson_category_transient_flusher' );
 add_action( 'save_post',     'marcus_thompson_category_transient_flusher' );
+
+/**
+ * Social media icon menu
+ */
+function marcus_thompson_social_menu() {
+    if ( has_nav_menu( 'social' ) ) {
+	wp_nav_menu(
+		array(
+			'theme_location'  => 'social',
+			'container'       => 'div',
+			'container_id'    => 'menu-social',
+			'container_class' => 'menu-social',
+			'menu_id'         => 'menu-social-items',
+			'menu_class'      => 'menu-items',
+			'depth'           => 1,
+			'link_before'     => '<span class="screen-reader-text">',
+			'link_after'      => '</span>',
+			'fallback_cb'     => '',
+		)
+	);
+    }
+}
