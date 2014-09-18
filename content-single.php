5a1be821c302b273a75b6797da84ea82a6d615f2
Marcus Thompson <marcust@email.unc.edu>
2014-09-18 16:45:00 -0400

    Modified single post template content structure.

--- content-single.php
+++ content-single.php
@@ -7,6 +7,14 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <?php
+                /* translators: used between list items, there is a space after the comma */
+                $category_list = get_the_category_list( __( ', ', 'my-simone' ) );
+
+                if (marcus_thompson_categorized_blog() ) {
+                    echo '<div class="category-list">' . $category_list . '</div>';
+                }
+            ?>
 
 		<div class="entry-meta">
 			<?php marcus_thompson_posted_on(); ?>
@@ -25,36 +33,8 @@
 
 	<footer class="entry-footer">
 		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$category_list = get_the_category_list( __( ', ', 'marcus-thompson' ) );
-
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'marcus-thompson' ) );
-
-			if ( ! marcus_thompson_categorized_blog() ) {
-				// This blog only has 1 category so we just need to worry about tags in the meta text
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'marcus-thompson' );
-				} else {
-					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'marcus-thompson' );
-				}
-			} else {
-				// But this blog has loads of categories so we should probably display them here
-				if ( '' != $tag_list ) {
-					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'marcus-thompson' );
-				} else {
-					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'marcus-thompson' );
-				}
-			} // end check for categories on this blog
-
-			printf(
-				$meta_text,
-				$category_list,
-				$tag_list,
-				get_permalink()
-			);
-		?>
-
+                    echo get_the_tag_list( '<ul><li><i class="fa fa-tag"></i>', '</li><li><i class="fa fa-tag"></i>', '</li></ul>' );
+                ?>
 		<?php edit_post_link( __( 'Edit', 'marcus-thompson' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
