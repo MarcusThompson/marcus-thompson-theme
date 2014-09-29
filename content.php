abfc286a9da21428df388d9f3d5ebca5e5d1698a
Marcus Thompson <marcust@email.unc.edu>
2014-09-29 15:13:22 -0400

    Created a custom display for the most recent post.

--- content.php
+++ content.php
@@ -5,23 +5,38 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <div class="index-box">
-            <?php 
+        <?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { // Custom template for the first post on the front page
+            if (has_post_thumbnail()) {
+                echo '<div class="front-index-thumbnail clear">';
+                echo '<div class="image-shifter">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">';
+                echo the_post_thumbnail('large-thumb');
+                echo '</a>';
+                echo '</div>';
+                echo '</div>';
+            } 
+            echo '<div class="index-box';
+            if (has_post_thumbnail()) { echo ' has-thumbnail'; };
+            echo '">';
+        } else {
+            echo '<div class="index-box">';
             if (has_post_thumbnail()) {
                 echo '<div class="small-index-thumbnail clear">';
-                echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">';
+                echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">';
                 echo the_post_thumbnail('index-thumb');
                 echo '</a>';
                 echo '</div>';
             }
-            ?>
+        }
+        ?>
 	<header class="entry-header">
             
             <?php 
                 if (is_sticky ()) {
                     echo '<i class="fa fa-thumb-tack sticky-post"></i>';
                 }
-            ?>    
+            ?>
             
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
@@ -40,17 +55,21 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'marcus-thompson' ),
-				'after'  => '</div>',
-			) );
-		?>
-	<footer class="entry-footer continue-reading">
-    <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
-</footer><!-- .entry-footer -->
-	</footer><!-- .entry-footer -->
+	<?php 
+        if( $wp_query->current_post == 0 && !is_paged() && is_front_page() ) { 
+            echo '<div class="entry-content">';
+            the_content( __( '', 'marcus-thompson' ) );
+            echo '</div>';
+            echo '<footer class="entry-footer continue-reading">';
+            echo '<a href="' . get_permalink() . '" title="' . __('Read ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">Read the article<i class="fa fa-arrow-circle-o-right"></i></a>'; 
+            echo '</footer><!-- .entry-footer -->';
+        } else { ?>
+            <div class="entry-content">
+            <?php the_excerpt(); ?>
+            </div><!-- .entry-content -->
+            <footer class="entry-footer continue-reading">
+            <?php echo '<a href="' . get_permalink() . '" title="' . __('Continue Reading ', 'marcus-thompson') . get_the_title() . '" rel="bookmark">Continue Reading<i class="fa fa-arrow-circle-o-right"></i></a>'; ?>
+            </footer><!-- .entry-footer -->
+<?php } ?>
         </div><!-- .index-box -->
 </article><!-- #post-## -->
