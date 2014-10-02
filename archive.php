1b717094a1641d983105b9b1b6d486a86cfcd98f
Marcus Thompson <marcust@email.unc.edu>
2014-10-02 17:30:59 -0400

    Configured the archive outputs.

--- archive.php
+++ archive.php
@@ -17,56 +17,38 @@ get_header(); ?>
 			<header class="page-header">
 				<h1 class="page-title">
 					<?php
-						if ( is_category() ) :
-							single_cat_title();
+                                        if ( is_category() ) :
+                                            printf( __( 'Posts in the ', 'marcus-thompson' ) );
+                                            echo '<em>';
+                                            single_cat_title();
+                                            echo '</em> ' . __('category', 'marcus-thompson') . ':';
 
-						elseif ( is_tag() ) :
-							single_tag_title();
+                                        elseif ( is_tag() ) :
+                                            printf( __( 'Posts with the ', 'marcus-thompson' ) );
+                                            echo '<em>';
+                                            single_tag_title();
+                                            echo '</em> ' . __('tag', 'marcus-thompson') . ':';
 
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'marcus-thompson' ), '<span class="vcard">' . get_the_author() . '</span>' );
+                                        elseif ( is_author() ) :
+                                            printf( __( 'Author: %s', 'marcus-thompson' ), '<span class="vcard">' . get_the_author() . '</span>' );
 
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'marcus-thompson' ), '<span>' . get_the_date() . '</span>' );
+                                        elseif ( is_day() ) :
+                                            printf( __( 'Posts from %s', 'marcus-thompson' ), '<span>' . get_the_date() . '</span>' );
 
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'marcus-thompson' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'marcus-thompson' ) ) . '</span>' );
+                                        elseif ( is_month() ) :
+                                            printf( __( 'Posts from %s', 'marcus-thompson' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'marcus-thompson' ) ) . '</span>' );
 
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'marcus-thompson' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'marcus-thompson' ) ) . '</span>' );
+                                        elseif ( is_year() ) :
+                                            printf( __( 'Posts from %s', 'marcus-thompson' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'marcus-thompson' ) ) . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'marcus-thompson' );
+                                        elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+                                            _e( 'Asides', 'marcus-thompson' );
 
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'marcus-thompson' );
+                                        else :
+                                            _e( 'Archives', 'marcus-thompson' );
 
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'marcus-thompson' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'marcus-thompson' );
-
-						else :
-							_e( 'Archives', 'marcus-thompson' );
-
-						endif;
-					?>
+                                        endif;
+                                        ?>
 				</h1>
 				<?php
 					// Show an optional term description.
